cd04f07d03d5d5df60ac0c94f3655b208e7be0e2
Rob Tice <rtice3@gmail.com>
2014-01-30 02:55:43 -0500

    Removed frameworks. Working with jquery UI and image slider

--- js/logic.js
+++ js/logic.js
@@ -1,109 +1,110 @@
 function home() {
-	$( "#aboutCarousel" ).empty().append(" \
-		<ol class='carousel-indicators'> \
-	   		<li data-target='#aboutCarousel' data-slide-to='0' class='active'></li> \
-	       	<li data-target='#aboutCarousel' data-slide-to='1'></li> \
-	       	<li data-target='#aboutCarousel' data-slide-to='2'></li> \
-	   	</ol> \
-	   	<div id='about-carousel-inner' class='carousel-inner'></div> \
-	   	<a class='left carousel-control' href='#aboutCarousel' data-slide='prev'><span class='glyphicon glyphicon-chevron-left'></span></a> \
-	    <a class='right carousel-control' href='#aboutCarousel' data-slide='next'><span class='glyphicon glyphicon-chevron-right'></span></a> \
+	$( "#slider" ).empty().append("\
+		<img src='img/index/index_14.gif' alt='#about0' /> \
+		<img src='img/index/index_20.gif' alt='#about1' /> \
 	");
-	$( "#about-carousel-inner" ).empty().append(" \
-		<div class='item active'> \
-		   	<img data-src='js/holder.js/900x500/auto/#FFFFFF:#7a7a7a/text:First slide'> \
-			<div class='container'> \
-		  		<img src='img/excavating/excavating_10.gif'> \
-		      	<div class='carousel-caption'> \
-		        	<h1>Welcome to the Jones Family</h1> \
-		        	<p>Jones Contracting Incorporated is a family owned and operated excavation contracting company based in Walpole, MA.</p> \
-		       	</div> \
-		   	</div> \
-		</div> \
-		<div class='item'> \
-		 	<img data-src='js/holder.js/900x500/auto/#FFFFFF:#6a6a6a/text:Second slide'> \
-		   	<div class='container'> \
-		      	<div class='carousel-caption'> \
-		           	<p>We specialize in excavation site-work, utilities, spectic systems, drainage, asphalt paving, landscape construction, and snow removal services in Eastern Massachusetts.</p> \
-		       	</div> \
-		  	</div> \
-		</div> \
-		<div class='item'> \
-		   	<img data-src='js/holder.js/900x500/auto/#FFFFFF:#5a5a5a/text:Third slide'> \
-		  	<div class='container'> \
-		      	<div class='carousel-caption'> \
-		          	<p>Whether your project is a small residential septic systems or commercial land development our experience and skills lead to solid results. Jones Contracting has taken a variety of different projects with varying scopes of work in both the public and private sectors.</p> \
-		       	</div> \
-		  	</div> \
-		</div> \
+	$( "#captions" ).empty().append("\
+		<div id='about0' class='comment'></div>	\
+		<div id='about1' class='comment'></div> \
+	");
+	$( "#about0" ).empty().append("\
+		<h1>Welcome to the Jones Family</h1> \
+		<p>Jones Contracting Incorporated is a family owned and operated excavation contracting company based in Walpole, MA.</p> \
+		<p>We specialize in excavation site-work, utilities, spectic systems, drainage, asphalt paving, landscape construction, and snow removal services in Eastern Massachusetts.</p> \
+	");
+	$( "#about1" ).empty().append("\
+		<p>Whether your project is a small residential septic systems or commercial land development our experience and skills lead to solid results. Jones Contracting has taken a variety of different projects with varying scopes of work in both the public and private sectors.</p> \
+	");	
+}
+
+function excavating() {
+	$( "#slider" ).empty().append("\
+		<img src='img/excavating/excavating_10.gif' alt='#excavating0' /> \
+		<img src='img/excavating/excavating_20.gif' alt='#excavating1' /> \
+		<img src='img/excavating/excavating_22.gif' alt='#excavating2' /> \
+		<img src='img/excavating/excavating_24.gif' alt='#excavating3' /> \
+	");
+
+	$( "#captions" ).empty().append("\
+		<div id='excavating0' class='comment'></div>	\
+		<div id='excavating1' class='comment'></div> \
+		<div id='excavating2' class='comment'></div> \
+		<div id='excavating3' class='comment'></div> \
+	");
+	$( "#excavating0" ).empty().append("\
+		<h1>Excavation and Sitework</h1> \
+		<p><ul> \
+		    <li>Earth Moving</li> \
+		    <li>Site Utilities</li> \
+		    <li>Site Drainage</li> \
+		    <li>Retension and Detention Ponds</li> \
+		</ul></p> \
+	");
+	$( "#excavating1" ).empty().append("\
+		<p><ul> \
+		    <li>Bioswales</li> \
+		    <li>Rain Gardens</li> \
+		    <li>Road Construction</li> \
+		    <li>Subgrade Preparation</li> \
+		</ul></p> \
+	");
+	$( "#excating2" ).empty().append("\
+		<p><ul> \
+		    <li>Road Base Construction</li> \
+		    <li>Asphalt, Concrete, Granite Curb</li> \
+		    <li>Asphalt Paving</li> \
+		    <li>Concrete and Asphalt Sidewalks</li> \
+		</ul></p> \
 	");
 }
 
-function excavation() {
-	$( "#myCarousel" ).empty().append(" \
-		<ol class='carousel-indicators'> \
-	   		<li data-target='#myCarousel' data-slide-to='0' class='active'></li> \
-	       	<li data-target='#myCarousel' data-slide-to='1'></li> \
-	       	<li data-target='#myCarousel' data-slide-to='2'></li> \
-	   	</ol> \
-	   	<div id='carousel-inner' class='carousel-inner'></div> \
-	   	<a class='left carousel-control' href='#myCarousel' data-slide='prev'><span class='glyphicon glyphicon-chevron-left'></span></a> \
-	    <a class='right carousel-control' href='#myCarousel' data-slide='next'><span class='glyphicon glyphicon-chevron-right'></span></a> \
-	");
-	$( "#carousel-inner" ).empty().append(" \
-		<div class='item active'> \
-		   	<img data-src='js/holder.js/900x500/auto/#FFFFFF:#7a7a7a/text:First excavation'> \
-			<div class='container'> \
-		  		<img src='img/excavating/excavating_10.gif'> \
-		      	<div class='carousel-caption'> \
-		        	<h1>Excavation and Sitework</h1> \
-		        	<p><ul> \
-		        		<li>Earth Moving</li> \
-		        		<li>Site Utilities</li> \
-		        		<li>Site Drainage</li> \
-		        		<li>Retension and Detention Ponds</li> \
-		        	</ul></p> \
-		       	</div> \
-		   	</div> \
-		</div> \
-		<div class='item'> \
-		 	<img data-src='js/holder.js/900x500/auto/#FFFFFF:#6a6a6a/text:Second excavation'> \
-		   	<div class='container'> \
-		      	<div class='carousel-caption'> \
-		           	<p><ul> \
-		        		<li>Bioswales</li> \
-		        		<li>Rain Gardens</li> \
-		        		<li>Road Construction</li> \
-		        		<li>Subgrade Preparation</li> \
-		        	</ul></p> \
-		       	</div> \
-		  	</div> \
-		</div> \
-		<div class='item'> \
-		   	<img data-src='js/holder.js/900x500/auto/#FFFFFF:#5a5a5a/text:Third excavation'> \
-		  	<div class='container'> \
-		      	<div class='carousel-caption'> \
-		          	<p><ul> \
-		        		<li>Road Base Construction</li> \
-		        		<li>Asphalt, Concrete, Granite Curb</li> \
-		        		<li>Asphalt Paving</li> \
-		        		<li>Concrete and Asphalt Sidewalks</li> \
-		        	</ul></p> \
-		       	</div> \
-		  	</div> \
-		</div> \
-	");
-}
+var theme_list = [
+	"black-tie",
+	"blitzer",
+	"cupertino",
+	"dark-hive",
+	"dot-luv",
+	"eggplant",
+	"excite-bike",
+	"flick",
+	"hot-sneaks",
+	"humanity",
+	"le-frog",
+	"mint-choc",
+	"overcast",
+	"pepper-grinder",
+	"redmond",
+	"smoothness",
+	"south-street",
+	"start",
+	"sunny",
+	"swanky-purse",
+	"trontastic",
+	"ui-darkness",
+	"ui-lightness",
+	"vader"
+];
 
 
 $( document ).ready(function() {
-	home();
-	excavation();
-	$('.nav-tabs a').click(function (e) {
-      	e.preventDefault();
-      	$(this).tab('show');
-    });
+	$( "<link>" )
+			.appendTo($("head"))
+			.attr({type : "text/css", rel : "stylesheet"})
+			.attr("href" , "jquery-ui-1.10.4.custom/css/themes/south-street/jquery-ui.css");	
+			$( "#menu" ).hide();
+	$( "#tabs" ).tabs();
+	home();	
+
+	$("input:radio[name=radio]").click(function() {
+   	 	var path = "jquery-ui-1.10.4.custom/css/themes/".concat(theme_list[$("#radio1").prop("checked", true)],  "/jquery-ui.css");
+   	 	console.log("Path: ");
+   	 	console.log(path);
+			$( "<link>" )
+				.appendTo($("head"))
+				.attr({type : "text/css", rel : "stylesheet"})
+				.attr("href" , "jquery-ui-1.10.4.custom/css/themes/cupertino/jquery-ui.css");	
+				$( "#menu" ).hide();
+	});
+	
 });
-
-
 
