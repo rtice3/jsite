f705cdcb0f8a8e8aa6d0c6c02ae68f71dc60e3d7
Rob Tice <rtice3@gmail.com>
2014-02-05 20:39:06 -0500

    Cleaning
    Cleaning image files and uploaded new bigger files. Will have to crop
    to the correct size to save on bandwidth at image load. Also switched
    to jssor image slider. Its way better. Also I can do multiple instances
    on a page without rewriting it (thanks jslider). Also started to add
    google API for maps on contact page. Have to write form.

--- js/logic.js
+++ js/logic.js
@@ -1,14 +1,14 @@
 function home() {
-	$( "#slider" ).empty().append("\
+	$( "#aboutslider" ).empty().append("\
 		<img src='img/index/index_14.gif' alt='#about0' /> \
 		<img src='img/index/index_20.gif' alt='#about1' /> \
 	");
-	$( "#captions" ).empty().append("\
+	$( "#about_captions" ).empty().append("\
 		<div id='about0' class='comment'></div>	\
 		<div id='about1' class='comment'></div> \
 	");
 	$( "#about0" ).empty().append("\
-		<h1>Welcome to the Jones Family</h1> \
+		<h1>Welcome to Jones Contracting</h1> \
 		<p>Jones Contracting Incorporated is a family owned and operated excavation contracting company based in Walpole, MA.</p> \
 		<p>We specialize in excavation site-work, utilities, spectic systems, drainage, asphalt paving, landscape construction, and snow removal services in Eastern Massachusetts.</p> \
 	");
@@ -18,14 +18,14 @@ function home() {
 }
 
 function excavating() {
-	$( "#slider" ).empty().append("\
+	$( "#excavating_slider" ).empty().append("\
 		<img src='img/excavating/excavating_10.gif' alt='#excavating0' /> \
 		<img src='img/excavating/excavating_20.gif' alt='#excavating1' /> \
 		<img src='img/excavating/excavating_22.gif' alt='#excavating2' /> \
 		<img src='img/excavating/excavating_24.gif' alt='#excavating3' /> \
 	");
 
-	$( "#captions" ).empty().append("\
+	$( "#excavating_captions" ).empty().append("\
 		<div id='excavating0' class='comment'></div>	\
 		<div id='excavating1' class='comment'></div> \
 		<div id='excavating2' class='comment'></div> \
@@ -86,25 +86,63 @@ var theme_list = [
 ];
 
 
-$( document ).ready(function() {
+$( document ).ready(function($) {
 	$( "<link>" )
 			.appendTo($("head"))
 			.attr({type : "text/css", rel : "stylesheet"})
 			.attr("href" , "jquery-ui-1.10.4.custom/css/themes/south-street/jquery-ui.css");	
-			$( "#menu" ).hide();
-	$( "#tabs" ).tabs();
-	home();	
+			
+	$( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+    $( "#radiod" ).buttonset();
 
-	$("input:radio[name=radio]").click(function() {
-   	 	var path = "jquery-ui-1.10.4.custom/css/themes/".concat(theme_list[$("#radio1").prop("checked", true)],  "/jquery-ui.css");
+
+	$("input:radio[name=radio1]").click(function() {
+   	 	var path = "jquery-ui-1.10.4.custom/css/themes/".concat(theme_list[$("input[name='radio1']:checked").val()],  "/jquery-ui.css");
    	 	console.log("Path: ");
    	 	console.log(path);
 			$( "<link>" )
 				.appendTo($("head"))
 				.attr({type : "text/css", rel : "stylesheet"})
-				.attr("href" , "jquery-ui-1.10.4.custom/css/themes/cupertino/jquery-ui.css");	
-				$( "#menu" ).hide();
+				.attr("href" , path);	
+		
 	});
+
+	var _CaptionTransitions = [];
+            _CaptionTransitions["L"] = { $Duration: 800, $FlyDirection: 1, $Easing: $JssorEasing$.$EaseInCubic };
+            _CaptionTransitions["R"] = { $Duration: 800, $FlyDirection: 2, $Easing: $JssorEasing$.$EaseInCubic };
+            _CaptionTransitions["T"] = { $Duration: 800, $FlyDirection: 4, $Easing: $JssorEasing$.$EaseInCubic };
+            _CaptionTransitions["B"] = { $Duration: 800, $FlyDirection: 8, $Easing: $JssorEasing$.$EaseInCubic };
+            _CaptionTransitions["B|IE"] = {$Duration:800,$FlyDirection:8,$Easing:{$Top:$JssorEasing$.$EaseInOutElastic},$ScaleVertical:0.6,$Opacity:2};
+            _CaptionTransitions["TL"] = { $Duration: 800, $FlyDirection: 5, $Easing: $JssorEasing$.$EaseInCubic };
+            _CaptionTransitions["TR"] = { $Duration: 800, $FlyDirection: 6, $Easing: $JssorEasing$.$EaseInCubic };
+            _CaptionTransitions["BL"] = { $Duration: 800, $FlyDirection: 9, $Easing: $JssorEasing$.$EaseInCubic };
+            _CaptionTransitions["BR"] = { $Duration: 800, $FlyDirection: 10, $Easing: $JssorEasing$.$EaseInCubic };
+            _CaptionTransitions["WAVE|L"] = { $Duration: 1500, $FlyDirection: 5, $Easing: { $Left: $JssorEasing$.$EaseLinear, $Top: $JssorEasing$.$EaseOutWave }, $ScaleVertical: 0.4, $Round: { $Top: 2.5} };
+            _CaptionTransitions["MCLIP|B"] = { $Duration: 600, $Clip: 8, $Move: true, $Easing: $JssorEasing$.$EaseOutExpo };
+
+            var options = {
+                $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+                $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+                $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
+                    $Class: $JssorCaptionSlider$,                   //[Required] Class to create instance to animate caption
+                    $CaptionTransitions: _CaptionTransitions,       //[Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
+                    $PlayInMode: 1,                                 //[Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
+                    $PlayOutMode: 3                                 //[Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
+                }
+            };
+
+            var about_slider = new $JssorSlider$("about_container", options);
+      //      var gallery_slider = new $JssorSlider$("gallery_container", options);
+            var excavating_slider = new $JssorSlider$("excavating_container", options);
+       //     var utilities_slider = new $JssorSlider$("utilities_container", options);
+       //     var septic_slider = new $JssorSlider$("septic_container", options);
+       //     var asphalt_slider = new $JssorSlider$("asphalt_container", options);
+       //     var landscape_slider = new $JssorSlider$("landscape_container", options);
+       //     var snow_slider = new $JssorSlider$("snow_container", options);
+//            var technical_slider = new $JssorSlider$("technical_container", options);
+
+
 	
 });
 
