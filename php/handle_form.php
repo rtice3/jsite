b06322e42c5a64be870eacaf6b00cb745cd760a0
Rob Tice <rtice3@gmail.com>
2014-03-04 00:57:12 -0500

    Added php code to generate emails

--- php/handle_form.php
+++ php/handle_form.php
@@ -5,10 +5,40 @@ header("Pragma: no-cache");
 header("Content-Type: text/xml");
 
 $ip = $_SERVER['REMOTE_ADDR'];
-$msg = "Hello World to user from " . $ip . " at " . date("h:i:s A");
+$msg = "Hello World to user from " . $ip . " at ". date("h:i:s A");
 
 print "<?xml version='1.0' encoding='UTF-8'?>";
 print "<message>$msg</message>";
 
+if($_POST)
+{
+    if(!isset['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+		die();
+	}
 
+	$to_Email = "rtice3@gmail.com";
+
+	$name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+	$phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
+	$email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+	$subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+	$message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+    
+    
+    //proceed with PHP email.
+    $headers = 'From: '.$email.'' . "rn" .
+    'Reply-To: '.$email.'' . "rn" .
+    'X-Mailer: PHP/' . phpversion();
+    
+    @$sentMail = mail($to_Email, 'Form submission: '.$subject, $message .'  -'.$name, $headers);
+    
+    if(!$sentMail)
+    {
+        header('HTTP/1.1 500 Couldnot send mail! Sorry..');
+        exit();
+    }else{
+        echo 'Hi '.$name .', Thank you for your email! ';
+        echo 'Your email has already arrived in my Inbox, all I need to do is Check it.';
+    }
+}
 ?>
