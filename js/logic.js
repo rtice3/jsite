ab83c6bf237ebae5c709141cc9877192ef2a381f
Rob Tice <rtice3@gmail.com>
2014-03-13 19:35:49 -0400

    Updated browser detect script

--- js/logic.js
+++ js/logic.js
@@ -123,19 +123,39 @@ var _SlideshowTransitions = [
         $SlideDuration: 500
     };
 
-function isIE () {
-    var myNav = navigator.userAgent.toLowerCase();
-    console.log("myNav: " + myNav);
-    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+function getInternetExplorerVersion() {
+// Returns the version of Internet Explorer or a -1
+// (indicating the use of another browser).
+    var rv = -1; // Return value assumes failure.
+    if (navigator.appName == 'Microsoft Internet Explorer') {
+        var ua = navigator.userAgent;
+        var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat( RegExp.$1 );
+    }
+    return rv;
+}
+
+function checkVersion() {
+    var msg = "You're not using Internet Explorer.";
+    var ver = getInternetExplorerVersion();
+
+    if ( ver > -1 ) {
+        if ( ver >= 8.0 ) 
+            msg = "You're using a recent copy of Internet Explorer."
+    else
+        msg = "Welcome to Jones Contracting Inc. To view this website you must upgrade your browser. Please contact (508) 668-7888 for immediate assistance.";
+        alert(msg);
+        window.location="http://windows.microsoft.com/en-us/internet-explorer/download-ie";
+    }
+    
+    console.log(msg);
 }
 
 $( document ).ready(function($) {
-    var ie = isIE();
-    console.log("Browser Version: " + ie);
-    if( ie < 9 && ie > 0 ) {
-        alert("Welcome to Jones Contracting Inc. To view this website you must upgrade your browser. Please contact (508) 668-7888 for immediate assistance.");
-        window.location="http://windows.microsoft.com/en-us/internet-explorer/download-ie";
-    }
+
+    checkVersion();
+    
     $( "#about" ).load("html/home.html");
     
 //    $( "#gallery_container" ).hide();
