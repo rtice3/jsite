ebc0d4a2487b0534cf1777b2d9095b8905aabdac
Rob Tice <rtice3@gmail.com>
2014-03-13 00:18:32 -0400

    Fixing php alerts and redirects.

--- php/index.php
+++ php/index.php
@@ -3,12 +3,16 @@
 $browser = get_browser();
 
 if($browser->browser == 'IE' && $browser->majorver < 9) {
-	alert("Welcome to Jones Contracting Inc. To view this website you must upgrade your browser. Please contact (508) 668-7888 for immediate assistance.");
+	echo '<script language="javascript">';
+	echo 'alert("Welcome to Jones Contracting Inc. To view this website you must upgrade your browser. Please contact (508) 668-7888 for immediate assistance.")';
+	echo '</script>';
 	header( 'Location: http://windows.microsoft.com/en-us/internet-explorer/download-ie' );
 }
 else {
-	alert("Your browser works.");
-	readfile("index.html");
+	echo '<script language="javascript">';
+	echo 'alert("Your browser works.")';
+	echo '</script>';
+	header( "index.html" );
 }
 
 ?>
