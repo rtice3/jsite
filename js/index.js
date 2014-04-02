2052d1955ac1a3aad2b4b07cefd528768b14ad0b
Rob Tice <rtice3@gmail.com>
2014-04-02 01:51:01 -0400

    Moving to static div.

--- js/index.js
+++ js/index.js
@@ -54,15 +54,14 @@ function load_script(url, type, callback) {
 window.onload = function() {
     var hide = document.getElementById("hide_me");
     hide.parentNode.removeChild(hide);
-    var footer = document.getElementById("footer_container");
-    var loading_div = document.createElement('div');
-    loading_div.className = 'spinner';
-    footer.insertBefore(loading_div, footer);
-    loading_div.innerHTML = '<div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div>';
+    
+    var loading_div = document.getElementById("loading");
+    loading_div.innerHTML = '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
 
     checkVersion();
 
     load_script("js/jquery-1.10.2.min.js", "js", function() {
         $( "#wrap" ).load("html/layout.html");
+        loading_div.parentNode.removeChild(loading_div);
     });
 };
