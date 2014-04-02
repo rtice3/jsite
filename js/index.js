1b68c38b1afb28bbb895ee9304cab4b5591b5b3e
Rob Tice <rtice3@gmail.com>
2014-04-02 01:09:08 -0400

    Adding loading spinners on main loading page. Havent put code in to get rid of the spinner after load.

--- js/index.js
+++ js/index.js
@@ -54,6 +54,14 @@ function load_script(url, type, callback) {
 window.onload = function() {
     var hide = document.getElementById("hide_me");
     hide.parentNode.removeChild(hide);
+    var loading = document.createElement('div');
+    loading.setAttribute('id', 'loading_div');
+    var loading_div = document.getElementById("loading_div");
+    loading_div.innerHTML = '<div class="spinner"> \
+                                <div class="bounce1"></div> \
+                                <div class="bounce2"></div> \
+                                <div class="bounce3"></div> \
+                            </div>'
 
     checkVersion();
 
