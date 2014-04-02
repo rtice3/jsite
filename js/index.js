e045e52aaaac6da33ab8feeefd8dcedf3196ade2
Rob Tice <rtice3@gmail.com>
2014-04-02 01:40:29 -0400

    Didn't realize i had to pass an element into insertbefore.

--- js/index.js
+++ js/index.js
@@ -55,9 +55,10 @@ window.onload = function() {
     var hide = document.getElementById("hide_me");
     hide.parentNode.removeChild(hide);
     var footer = document.getElementById("footer_container");
-    var loading_div = '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
+    var loading_div = document.createElement("div");
     footer.insertBefore(loading_div, footer);
-    
+    loading_div.innerHTML = '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
+
     checkVersion();
 
     load_script("js/jquery-1.10.2.min.js", "js", function() {
