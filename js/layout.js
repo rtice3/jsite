671e8b9fba08090764b91df100ac86e48b473a43
Rob Tice <rtice3@gmail.com>
2014-04-02 02:21:08 -0400

    Adding multiple color classes.

--- js/layout.js
+++ js/layout.js
@@ -18,13 +18,17 @@ function close_gallery() {
 function load_tab(callback) {
     if(oneshot[newTab] == 0) {
         oneshot[newTab] = 1;
+        $( "#" + tab_index[newTab] ).html('<div class="spinner spinner-dark"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>');
         if(callback) {
             $( "#" + tab_index[newTab] ).load("html/" + tab_index[newTab] + ".html", function() {
                 basic_tab(tab_index[newTab]);
+                $( ".spinner" ).remove();
             });
         }
         else {
-            $( "#" + tab_index[newTab] ).load("html/" + tab_index[newTab] + ".html");
+            $( "#" + tab_index[newTab] ).load("html/" + tab_index[newTab] + ".html", function() {
+                $( ".spinner" ).remove();
+            });
         }
     }
 }
