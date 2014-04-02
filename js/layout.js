b933b3c4f3a5ebe1bbbf5cabd9a18b9ef9bc4495
Rob Tice <rtice3@gmail.com>
2014-04-02 09:44:50 -0400

    Fixing css.

--- js/layout.js
+++ js/layout.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
             oldTab = ui.oldTab.index();
             newTab = ui.newTab.index();
             if(newTab == 1) {
-                $( "#gallery_wrap" ).html('<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>');
+                $( "#gallery_wrap" ).html('<div class="spinner" style="position:fixed;top:50%;left:50%;"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>');
                 gallery_open = true;
                 $( "#gallery_wrap" ).load("html/" + tab_index[newTab] + ".html");
             }
