ce265b3c0e4707d250040b4dddc07353034b0306
Rob Tice <rtice3@gmail.com>
2015-02-08 18:23:16 -0500

    Switching to json content.

--- js/angular_ctrl.js
+++ js/angular_ctrl.js
@@ -39,55 +39,21 @@
     }
   });
 
-  app.controller('about_ctrl', function() {
-    this.carousel_data = about_carousel;
-    this.marketing_data = about_marketing;
-  });
+  app.controller('about_ctrl', [ '$http', function($http) {
+    var store = this;
+    store.carousel_data = [ ];
+    store.marketing_data = [ ];
+    $http.get('/json/about_carousel.json').success(function(data) {
+      store.carousel_data = data;
+    });
+    $http.get('/json/about_marketing.json').success(function(data) {
+      store.marketing_data = data;
+    });
+  }]);
 
   app.controller('contact_ctrl', function() {
 
   });
 
-  var about_carousel = [{
-      img_src: "img/home/home_00.jpeg",
-      header_txt: "Welcome to Jones Contracting Inc.",
-      caption_txt: "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.",
-      btn_dest: "#contact",
-      btn_txt: "Contact Us",
-    }, {
-      img_src: "img/home/home_01.jpeg",
-      header_txt: "Residential Services",
-      caption_txt: "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.",
-      btn_dest: "#residential",
-      btn_txt: "Residential Services",
-    }, {
-      img_src: "img/home/home_07.jpeg",
-      header_txt: "Municipal & State Services",
-      caption_txt: "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.",
-      btn_dest: "#municipal",
-      btn_txt: "Municipal & State Services",
-  }];
+});
 
-  var about_marketing = [{
-      img_src: "img/construction11.png",
-      header_txt: "Local",
-      body_txt: "Jones Contracting, Inc. is a family owned and operated excavation company offering services in excavation and site work, septic system, utilities, asphalt paving, landscape construction, and snow plowing and removal. Located in Walpole, MA, we take on residential, commercial and public projects across Eastern Massachusetts.",
-      btn_dest: "#",
-      btn_txt: "View details",
-    },
-    {
-      img_src: "img/bulldozer2.png",
-      header_txt: "Quality",
-      body_txt: "Our team is made up of seasoned professionals, including our experienced crew and degreed engineers. Jones Contracting, Inc. is licensed, insured, and bonded for any of your excavation service needs. We are committed to superior quality work, customer service, and attention to detail. We believe in serving our clients without cutting corners.",
-      btn_dest: "#",
-      btn_txt: "View details",
-    },
-    {
-      img_src: "img/truck29.png",
-      header_txt: "Service",
-      body_txt: "Please browse our site for information about our work and photographs of our past projects. We are glad to provide free estimates and quotes for any of your excavation needs - big or small.</p><p>We look forward to working with you.",
-      btn_dest: "#",
-      btn_txt: "View details",
-  }];
-
-})();
