f70935151f7a818abb968a307e308782c965d51c
Rob Tice <rtice3@gmail.com>
2015-02-08 16:53:28 -0500

    Fixing maps.

--- js/contact.js
+++ js/contact.js
@@ -28,10 +28,4 @@ function initialize() {
     infowindow.open(map,marker);
   });
 }
-function loadScript() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-    document.body.appendChild(script);
-}
-loadScript();
+initialize();
