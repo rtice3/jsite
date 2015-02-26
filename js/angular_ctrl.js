90c5bb629d72cf637c67d6e237390c7cd50fde47
Rob Tice <rtice3@gmail.com>
2015-02-25 23:48:42 -0500

    Adding animations to slide transitions.

--- js/angular_ctrl.js
+++ js/angular_ctrl.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('angular_app', ['ui.bootstrap', 'ngRoute']);
+  var app = angular.module('angular_app', ['ui.bootstrap', 'ngRoute', 'ngAnimate']);
 
   app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
