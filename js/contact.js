5a58552e7652c11e54d02620c93d8b657a0c1cab
Rob Tice <rtice3@gmail.com>
2015-04-02 01:24:56 -0400

    The fight with facebook continues.

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,20 @@
-function initialize() {
+function init_fb(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) 
+        return;
+    js = d.createElement(s); 
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1588258464750206&version=v2.3";
+    fjs.parentNode.insertBefore(js, fjs);
+}
+
+function init_gp() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+}
+
+function init_map() {
     var myLatlng = new google.maps.LatLng(42.144991, -71.232199);
     var mapOptions = {
         zoom: 9,
@@ -28,4 +44,6 @@ function initialize() {
     infowindow.open(map,marker);
   });
 }
-initialize();
+init_map();
+init_gp();
+init_fb(document, 'script', 'facebook-jssdk');
